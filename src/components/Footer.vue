edac80a72e872a4334e70f3b453ee56bbf38bc36
yudeshui <dyuu0013@student.monash.edu>
2024-08-26 20:00:23 +1000

    finish the web footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,91 +1,62 @@
 <template>
-  <nav class="navbar navbar-expand-md">
+  <footer class="bg-dark text-light pt-5">
     <div class="container">
-      <a class="navbar-brand" href="#">Logo</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Activities</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">News</a>
-          </li>
-          <div class="separator"></div>
-          <li class="nav-item">
-            <a class="btn btn-register" href="#">Register</a>
-          </li>
-          <li class="nav-item">
-            <a class="btn btn-login" href="#">Log In</a>
-          </li>
-        </ul>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="align-items-center mb-3">
+            <img src="../images/logo_activeaging.svg" alt="Logo" height="60" />
+          </div>
+          <div>
+            <p class="about_text">
+              About us : <br />ActiveAging is an online platform offering activities for seniors
+              to<br />
+              combat loneliness, foster social connections.
+            </p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <h5 class="text-uppercase">Suport Resources</h5>
+          <ul class="list-unstyled">
+            <li><a href="#" class="text-light">Blog</a></li>
+            <li><a href="#" class="text-light">Guidance</a></li>
+            <li><a href="#" class="text-light">Customer Stories</a></li>
+            <li><a href="#" class="text-light">Support</a></li>
+            <li><a href="#" class="text-light">Join in us</a></li>
+            <li><a href="#" class="text-light">Monash University</a></li>
+          </ul>
+        </div>
+        <div class="col-md-4">
+          <h5 class="text-uppercase">How to Contact Us</h5>
+          <div>
+            <img src="../images/address.svg" alt="Logo" height="20" style="margin-right: 4px" />
+            Address: Monash University, Clayton Campus Wellington Road, Clayton VIC 3800, Melbourne,
+            Australia.
+          </div>
+          <div style="margin-top: 10px; margin-bottom: 10px">
+            <p>
+              <img
+                src="../images/phone.svg"
+                alt="Logo"
+                height="20"
+                style="margin-right: 4px"
+              />Phone : 183290384721
+            </p>
+          </div>
+          <div class="info-logo">
+            <img src="../images/wechat.svg" alt="Logo" height="30" style="margin-right: 20px" />
+            <img src="../images/instergram.svg" alt="Logo" height="30" style="margin-right: 20px" />
+            <img src="../images/tiktok.svg" alt="Logo" height="30" style="margin-right: 20px" />
+            <img src="../images/facebook.svg" alt="Logo" height="30" style="margin-right: 20px" />
+          </div>
+        </div>
+      </div>
+      <div class="row mt-4">
+        <div class="col text-center">
+          <p>&copy; Deshui Yu. All rights reserved.</p>
+        </div>
       </div>
     </div>
-  </nav>
+  </footer>
 </template>
 
-<style scoped>
-.navbar {
-  height: 80px;
-}
-.navbar-brand,
-.nav-link,
-.btn {
-  font-size: 1.2rem; /* 增大文字大小以提高可读性 */
-}
-.nav-item {
-  margin-right: 26px;
-}
-
-.btn-login {
-  background-color: #28a745;
-  border-color: #28a745;
-  padding-left: 20px;
-  padding-right: 20px;
-  color: aliceblue;
-}
-.nav-item .btn-register {
-  margin-right: -14px;
-}
-.btn-login {
-  background-color: #28a745; /* Bootstrap的默认绿色 */
-  border-color: #28a745;
-  padding-left: 20px;
-  padding-right: 20px;
-  color: aliceblue;
-}
-.nav-link:hover {
-  color: #28a745;
-}
-
-.btn-register:hover {
-  background-color: #28a7463e;
-  color: #28a745;
-}
-.btn-login:hover {
-  background-color: #218838;
-  border-color: #218838;
-  box-shadow: 0px 4px 8px rgba(40, 167, 69, 0.5);
-  color: aliceblue;
-}
-.separator {
-  width: 2px;
-  height: 18px;
-  background-color: #d3d3d3;
-  margin-top: 13px;
-  margin-right: 10px;
-}
-</style>
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,19 @@
 <template>
   <div><NavBar /></div>
+  <div><Footer /></div>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue'
+import Footer from '@/components/Footer.vue'
 
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: 'Home',
   components: {
-    NavBar
+    NavBar,
+    // eslint-disable-next-line vue/no-reserved-component-names
+    Footer
   }
 }
 </script>
