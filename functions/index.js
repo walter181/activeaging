dac8c326375fa1b3da62696040ccd9186aaf07a9
yudeshui <dyuu0013@student.monash.edu>
2024-10-09 19:08:23 +1100

    send email

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,9 @@
 const { onRequest } = require('firebase-functions/v2/https')
 const admin = require('firebase-admin')
 const cors = require('cors')({ origin: true })
+const sgMail = require('@sendgrid/mail')
 
 admin.initializeApp()
-
 // Count events
 exports.countEvents = onRequest((req, res) => {
   cors(req, res, async () => {
@@ -46,6 +46,32 @@ exports.countBookings = onRequest((req, res) => {
     } catch (error) {
       console.error('Error counting bookings:', error.message) // Updated error message
       res.status(500).send('Error counting bookings') // Updated error message
+    }
+  })
+})
+
+exports.sendemail = onRequest((req, res) => {
+  // 在最开始处理 CORS
+  cors(req, res, async () => {
+    const { email } = req.body
+    if (!email) {
+      return res.status(400).send({ message: 'Email is required' })
+    }
+    console.log(email)
+    try {
+      const msg = {
+        to: email,
+        from: 'xtan0108@gmail.com',
+        subject: 'Sending with SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>'
+      }
+
+      await sgMail.send(msg)
+      res.status(200).send({ message: 'Email sent successfully' })
+    } catch (error) {
+      console.error('Error sending email:', error.message)
+      res.status(500).send('Error sending email')
     }
   })
 })
